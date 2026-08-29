2c30de90471f97d13eb75585ae16fbf0c8792d90
Gustavo Mendo <gustavomendo22pt@gmail.com>
2026-08-30 00:38:38 +0100

    Fase 1: modelo Cliente com construtor, validacao e ToString

---
+++ src/MiniCrm.ConsoleApp/Modelos/Cliente.cs
@@ -0,0 +1,28 @@
+﻿namespace MiniCrm.ConsoleApp.Modelos;
+
+internal class Cliente
+{
+    public Guid Id { get;}
+    public string Nome { get; private set; }
+    public string Email { get; set; }
+    public DateOnly DataRegisto { get; }
+
+    //Constructor
+    public Cliente(string nome, string email, DateOnly dataRegisto)
+    {
+        if (string.IsNullOrWhiteSpace(nome))
+        {
+            throw new ArgumentException("O nome do cliente não pode ser vazio.", nameof(nome));
+        }
+        Id = Guid.NewGuid();
+        Nome = nome;
+        Email = email;
+        DataRegisto = dataRegisto;
+    }
+
+    public override string ToString()
+    {
+        return $"ID: {Id}\nNome: {Nome}\nEmail: {Email}\nData de Registo: {DataRegisto}\n";
+
+    }
+}
--- src/MiniCrm.ConsoleApp/Program.cs
+++ src/MiniCrm.ConsoleApp/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using MiniCrm.ConsoleApp.Modelos;
+
+List<Cliente> lista = new List<Cliente>()
+{
+   
+};
+
+var cliente1 = new Cliente("João Silva", "joao.silva@gmail.com", new DateOnly(2023, 1, 15));
+var cliente2 = new Cliente("Maria Santos", "maria.santos@gmail.com", new DateOnly(2023, 2, 20));
+var cliente3 = new Cliente("Pedro Costa", "pedro.costa@gmail.com", new DateOnly(2023, 3, 10));
+lista.Add(cliente1);
+lista.Add(cliente2);
+lista.Add(cliente3);
+foreach (Cliente cliente in lista)
+{
+    Console.WriteLine(cliente);
+}
